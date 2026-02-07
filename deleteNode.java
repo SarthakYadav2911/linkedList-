aaaa038b11e1d9563276b270a007b22b338e831c
SarthakYadav2911 <sarthakyadav2911@gmail.com>
2026-02-07 16:35:50 +0530

    deleting a node from LinkedList

---
+++ deleteNode.java
@@ -0,0 +1,28 @@
+class Node
+{
+    int data;
+    Node next;
+
+    Node( int data1, Node next1    )
+    {
+        this.data= data1;
+        this.next = next1;
+    }
+
+    Node(int data1)
+    {
+        this.data= data1;
+        this.next = null;
+
+    }
+
+    public class deleteNode {
+    
+        public static void main(String [] args)
+        {
+            int arr[] = { 2,5,6,8};
+            Node y = new Node(arr[0]);
+            System.out.println(y.data);
+        }
+    }
+}
